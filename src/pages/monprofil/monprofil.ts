5eb2a16a40b5d5862c93122a023ab1f0889ec0be
CelinePaniz <celine.paniz@gmail.com>
2017-01-19 09:19:33 +0100

    Emply_v1.0.2
    Ajout des alertes du profil

--- src/pages/monprofil/monprofil.ts
+++ src/pages/monprofil/monprofil.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /*
   Generated class for the Monprofil page.
@@ -11,7 +12,7 @@ import { NavController, NavParams } from 'ionic-angular';
   selector: 'page-monprofil',
   templateUrl: 'monprofil.html'
 })
-export class MonprofilPage {
+/*export class MonprofilPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
@@ -19,4 +20,89 @@ export class MonprofilPage {
     console.log('Hello MonProfil Page');
   }
 
+}*/
+
+
+export class MonprofilPage {
+  constructor(public alertCtrl: AlertController) {
+  }
+
+  doChangementMDP() {
+    let prompt = this.alertCtrl.create({
+      title: 'Modification de mon mot de passe',
+      message: "Entrez votre mot de passe : ",
+      inputs: [
+        {
+          id: 'mdpAct',
+          name: 'mdpActuel',
+          placeholder: 'Mot de passe actuel'
+        },
+        {
+          id: 'NewMdp1',
+          name: 'mdpNew',
+          placeholder: 'Nouveau Mot de passe'
+        },
+        {
+          id: 'NewMdp2',
+          name: 'mdpNew2',
+          placeholder: 'Confirmer votre nouveau Mot de passe'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: data => {
+            console.log('Annulation de la modification');
+          }
+        },
+        {
+          text: 'Confirmer',
+          handler: data => {
+            console.log('Changement confirmé');
+            this.doCheck();
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+  doCheck(){
+    console.log('doCheck');
+    var MDP1 = document.getElementById("NewMdp1");
+    var MDP2 = document.getElementById("NewMdp2");
+    if (MDP1 == MDP2) {
+      console.log('true');
+      return true;
+    } else {
+      console.log('false');
+      return false;
+    }
+  }
+  doChangementMail() {
+    let prompt = this.alertCtrl.create({
+      title: 'Modification du mail',
+      message: "Entrez votre nouvelle adresse email : ",
+      inputs: [
+        {
+          name: 'newMail',
+          placeholder: 'Nouvelle adresse Email'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: data => {
+            console.log('Annulation de la modification');
+          }
+        },
+        {
+          text: 'Confirmer',
+          handler: data => {
+            console.log('Changement confirmé');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
