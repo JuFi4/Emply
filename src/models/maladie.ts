5d710c0aed6b41e420b2c05d8705ccb76b5fe6dd
VirtuElle <virtuelle@outlook.com>
2017-04-20 02:23:54 +0200

    Emply_v1.6.11
    Ficher oublié

--- src/models/maladie.ts
+++ src/models/maladie.ts
@@ -3,11 +3,15 @@ export class Maladie {
     dateDebut : Date;
     dateFin : Date;
     isAccident : boolean;
+    affichageDateDebut : string;
+    affichageDateFin : string;
 
     constructor(id, dateDebut : Date, dateFin : Date, isAccident){
         this.id = id; 
         this.dateDebut = dateDebut, 
         this.dateFin = dateFin; 
         this.isAccident = isAccident; 
+        this.affichageDateDebut = ('0' + dateDebut.getDate()).slice(-2) + "-"+ ('0' + (dateDebut.getMonth() + 1)).slice(-2) + "-" + dateDebut.getFullYear();
+        this.affichageDateFin = ('0' + dateFin.getDate()).slice(-2) + "-"+ ('0' + (dateFin.getMonth() + 1)).slice(-2) + "-" + dateFin.getFullYear();
     }//constructor
 }//Maladie
